b9cf937668c185861e65688b1835513105ddbf3a
Andi Purnawan <mail@andipurnawan.com>
2023-11-18 16:28:42 +0700

    enhancements-8

--- app/locales/index.ts
+++ app/locales/index.ts
@@ -47,7 +47,21 @@ export const AllLangs = Object.keys(ALL_LANGS) as Lang[];
 export const ALL_LANG_OPTIONS: Record<Lang, string> = {
   cn: "简体中文",
   en: "English",
+  tw: "繁體中文",
+  jp: "日本語",
+  ko: "한국어",
   id: "Indonesia",
+  fr: "Français",
+  es: "Español",
+  it: "Italiano",
+  tr: "Türkçe",
+  de: "Deutsch",
+  vi: "Tiếng Việt",
+  ru: "Русский",
+  cs: "Čeština",
+  no: "Nynorsk",
+  ar: "العربية",
+  bn: "বাংলা",
 };
 
 const LANG_KEY = "lang";
