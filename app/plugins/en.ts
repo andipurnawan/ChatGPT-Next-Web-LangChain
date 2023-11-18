ab3d938f6a679b4f73d1f7715e8c702aeae71103
Andi Purnawan <mail@andipurnawan.com>
2023-11-18 15:55:08 +0700

    enhancements-2

--- app/plugins/en.ts
+++ app/plugins/en.ts
@@ -1,13 +1,13 @@
 import { BuiltinPlugin } from "./typing";
 
 export const EN_PLUGINS: BuiltinPlugin[] = [
-  
+
   {
     name: "DALL·E",
     toolName: "dalle_image_generator",
     lang: "en",
     description:
-      "DALL·E 2 is an AI system that can create realistic images and art from a description in natural language. Using this plugin requires configuring Cloudflare R2 object storage service.",
+      "DALL·E 2 is an AI system that can create realistic images and art from a description in natural language.",
     builtin: true,
     createdAt: 1694703673000,
     enable: false,
@@ -17,7 +17,7 @@ export const EN_PLUGINS: BuiltinPlugin[] = [
     toolName: "stable_diffusion_image_generator",
     lang: "en",
     description:
-      "Stable Diffusion text-to-image model. Using this plugin requires configuring Cloudflare R2 object storage service and stable-diffusion-webui API.",
+      "Stable Diffusion text-to-image model.",
     builtin: true,
     createdAt: 1688899480510,
     enable: false,
