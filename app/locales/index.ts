61a0658cacdab721c7166b796532dd0bb2f9fb2e
Andi Purnawan <mail@andipurnawan.com>
2023-11-18 16:19:37 +0700

    enhancements-6

--- app/locales/index.ts
+++ app/locales/index.ts
@@ -1,11 +1,27 @@
+import cn from "./cn";
 import en from "./en";
+import tw from "./tw";
 import id from "./id";
+import fr from "./fr";
+import es from "./es";
+import it from "./it";
+import tr from "./tr";
+import jp from "./jp";
+import de from "./de";
+import vi from "./vi";
+import ru from "./ru";
+import no from "./no";
+import cs from "./cs";
+import ko from "./ko";
+import ar from "./ar";
+import bn from "./bn";
 import { merge } from "../utils/merge";
 
 import type { LocaleType } from "./cn";
 export type { LocaleType, PartialLocaleType } from "./cn";
 
 const ALL_LANGS = {
+  cn,
   en,
   id,
 };
@@ -15,6 +31,7 @@ export type Lang = keyof typeof ALL_LANGS;
 export const AllLangs = Object.keys(ALL_LANGS) as Lang[];
 
 export const ALL_LANG_OPTIONS: Record<Lang, string> = {
+  cn: "简体中文",
   en: "English",
   id: "Indonesia",
 };
